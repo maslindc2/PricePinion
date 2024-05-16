001fc91f21ba8a0b27fb6e054c3e05c31cb8c8d6
maslindc2 <maslindc2@users.noreply.github.com>
2024-05-16 19:04:29 +0000

    Prettified Code!

--- tests/routes/Route.test.ts
+++ tests/routes/Route.test.ts
@@ -1,11 +1,11 @@
-import chai, { assert, expect } from 'chai';
-import chaiHTTP from 'chai-http';
-import 'mocha';
-import { AnyObject } from 'mongoose';
+import chai, { assert, expect } from "chai";
+import chaiHTTP from "chai-http";
+import "mocha";
+import { AnyObject } from "mongoose";
 
 chai.use(chaiHTTP);
 
-describe('Get All Products', () => {
+describe("Get All Products", () => {
     let response: ChaiHttp.Response;
     // Before all tests make a call to our server to get all products and store the response
     before((done) => {
@@ -15,12 +15,12 @@ describe('Get All Products', () => {
                 response = res;
                 expect(res).to.have.status(200);
                 done();
-        });
+            });
     });
 
     // Check that the response we stored has a populated array
-    it('Response should successfully return a populated array', () => {
-        // Expect the response to be successful 
+    it("Response should successfully return a populated array", () => {
+        // Expect the response to be successful
         expect(response).to.have.status(200);
         // Expect the response.body to be an instance of Array as we get a JSON Array as a response
         expect(response.body).to.be.instanceof(Array);
@@ -29,67 +29,69 @@ describe('Get All Products', () => {
         // Expect the response to have headers
         expect(response).to.have.headers;
     });
-    
-    it('Product array should consist of Object with required properties', () => {
+
+    it("Product array should consist of Object with required properties", () => {
         expect(response.body).to.satisfy((products: any[]) => {
             // Iterate through the products array and check that each object satisfies the correct properties
             for (let i = 0; i < products.length; i++) {
-                // Since each index contains a product object we expect it to be of type Object 
+                // Since each index contains a product object we expect it to be of type Object
                 expect(products[i]).to.be.instanceOf(Object);
 
                 // Expect the current product to have the property productID
-                expect(products[i]).to.have.property('productID');
+                expect(products[i]).to.have.property("productID");
                 // Expect the productID to be a string
                 expect(products[i].productID).to.be.string;
                 // Expect the productID to not be an empty string
                 expect(products[i].productID).to.not.equal("");
-                
+
                 // Expect the current product to have the property productName
-                expect(products[i]).to.have.property('productName');
+                expect(products[i]).to.have.property("productName");
                 // Expect the productName to be a string
                 expect(products[i].productName).to.be.string;
                 // Expect the productName to not be an empty string
                 expect(products[i].productName).to.not.equal("");
-                
+
                 // Expect the current product to have the property storeName
-                expect(products[i]).to.have.property('storeName');
+                expect(products[i]).to.have.property("storeName");
                 // Expect the storeName to be a string
                 expect(products[i].storeName).to.be.string;
                 // Expect the storeName to not be an empty string
                 expect(products[i].storeName).to.not.equal("");
 
                 // Expect the current product to have the property productPrice
-                expect(products[i]).to.have.property('productPrice');
+                expect(products[i]).to.have.property("productPrice");
                 // Expect the productPrice to be a string
                 expect(products[i].productPrice).to.be.string;
                 // Expect the productPrice to not be an empty string
                 expect(products[i].productPrice).to.not.equal("");
 
                 // Expect the current product to have the property productLink
-                expect(products[i]).to.have.property('productLink');
+                expect(products[i]).to.have.property("productLink");
                 // Expect the productLink to be a string
                 expect(products[i].productLink).to.be.string;
                 // Expect the productLink to not be an empty string
                 expect(products[i].productLink).to.not.equal("");
 
                 // Expect the current product to have the property productImage
-                expect(products[i]).to.have.property('productImage');
+                expect(products[i]).to.have.property("productImage");
                 // Expect the productImage to be a string
                 expect(products[i].productImage).to.be.string;
                 // Expect the productImage to not be an empty string
                 expect(products[i].productImage).to.not.equal("");
-                
+
                 // Expect the current product to have the property productComparison
-                expect(products[i]).to.have.property('productComparison').that.is.instanceOf(Array);
+                expect(products[i])
+                    .to.have.property("productComparison")
+                    .that.is.instanceOf(Array);
             }
             return true;
         });
     });
 });
 
-describe('Get a Single Product', () => {
+describe("Get a Single Product", () => {
     let response: ChaiHttp.Response;
-    // Setting productID's of specific specific item we want to fetch 
+    // Setting productID's of specific specific item we want to fetch
     // This is the product id for Roma Tomatoes
     const productID = "fcd850d26cca1a70232123829fbec5ec";
 
@@ -113,14 +115,22 @@ describe('Get a Single Product', () => {
         expect(response).to.have.headers;
     });
     // Test for response structure on a unique item
-    it('Product array should consist of Object with required properties', () => {
+    it("Product array should consist of Object with required properties", () => {
         // Check that the product object contains all the expected properties
         expect(response.body).to.satisfy((product: Object) => {
             // Define the expected properties
-            const expectedProperties = ["productID", "productName", "storeName", "productPrice", "productLink", "productImage", "productComparison"];
-            for(const property in product){
+            const expectedProperties = [
+                "productID",
+                "productName",
+                "storeName",
+                "productPrice",
+                "productLink",
+                "productImage",
+                "productComparison",
+            ];
+            for (const property in product) {
                 // If the object contains a key that was not expected return false as this does not satisfy our requirements
-                if(!expectedProperties.includes(property)){
+                if (!expectedProperties.includes(property)) {
                     return false;
                 }
             }
@@ -133,7 +143,7 @@ describe('Get a Single Product', () => {
             expect(product.productID).to.be.string;
             // Expect the productID to NOT be an empty string
             expect(product.productID).to.not.equal("");
-            
+
             // Expect the productName to be a string
             expect(product.productName).to.be.string;
             // Expect the productName to NOT be an empty string
