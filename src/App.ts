73cd27a427eb07f6f53b6a93dc56eafcdcfe8148
prakhar28 <pajain96@gmail.com>
2024-05-15 21:59:25 -0700

    Fixed cors policies

--- src/App.ts
+++ src/App.ts
@@ -35,6 +35,7 @@ class App {
                 "Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept"
             );
+            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
             next();
         });
     }
