58e8386e7e17918afdd3eacd6f3ae862bebf3580
maslindc2 <maslindc2@users.noreply.github.com>
2024-09-29 19:07:39 +0000

    Prettified Code!

--- src/WebScraping/Utils/CreateBrowserInstance.ts
+++ src/WebScraping/Utils/CreateBrowserInstance.ts
@@ -1,7 +1,6 @@
 import puppeteer, { Browser } from "puppeteer";
 import UserAgent from "user-agents";
 
-
 class BrowserInstance {
     /**
      * Creating a browser instance.
@@ -10,14 +9,14 @@ class BrowserInstance {
      */
     public async createBrowserInstance(runHeadless: boolean): Promise<Browser> {
         // Generating a random desktop user agent from user-agents package
-        const userAgent = new UserAgent({deviceCategory: "desktop"});
+        const userAgent = new UserAgent({ deviceCategory: "desktop" });
 
         // Creating a browser instance using runHeadless boolean, hiding automation controlled flag, and setting a random user agent
         const browser = await puppeteer.launch({
             headless: runHeadless,
             args: [
-                '--disable-blink-features=AutomationControlled', 
-                `--user-agent=${userAgent.toString()}`, // Setting a random user agent 
+                "--disable-blink-features=AutomationControlled",
+                `--user-agent=${userAgent.toString()}`, // Setting a random user agent
             ],
         });
         // Return the browser instance we created.
