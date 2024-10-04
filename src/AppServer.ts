105a89fa2ef716eb527caded7cf5c192843593f7
Maslin Farrell <maslinfar@gmail.com>
2024-10-03 20:30:16 -0700

    Updating Product ID and removing NODE_ENV as it's managed separately

--- src/AppServer.ts
+++ src/AppServer.ts
@@ -5,14 +5,10 @@ import { logger } from "@logger";
 
 dotenv.config();
 
-const mongoURI = process.env.NODE_ENV === 'test'
-  ? process.env.MONGODB_TEST_URI // Use our Testing URI if test is set for node ENV
-  : process.env.MONGODB_URI;    // Use the production URI if NODE_ENV is not set to test
-
 // Defining the port number to use
 // Default is port 8080, you can set the env var to a custom port number
 const port = process.env.PORT;
-const mongoDBConnection = mongoURI;
+const mongoDBConnection = process.env.MONGODB_URI;
 
 const server = new App(mongoDBConnection).expressApp;
 server.listen(port);
